27bb550ea2bf3afc32f945342ec449d849a7120e
Eric Lindvall <eric@5stops.com>
2014-04-24 18:14:22 -0700

    Thread safety

--- lib/metriks/librato_metrics_reporter.rb
+++ lib/metriks/librato_metrics_reporter.rb
@@ -27,32 +27,50 @@ module Metriks
       else
         @percentiles = [ 0.95, 0.999 ]
       end
+
+      @mutex = Mutex.new
+      @running = false
     end
 
     def start
-      @thread ||= Thread.new do
-        while true
+      if @thread && @thread.alive?
+        return
+      end
+
+      @running = true
+      @thread = Thread.new do
+        while @running
           @time_tracker.sleep
 
           Thread.new do
-            begin
-              write
-            rescue Exception => ex
-              @on_error[ex] rescue nil
-            end
+            flush
           end
         end
       end
     end
 
     def stop
-      @thread.kill if @thread
-      @thread = nil
+      @running = false
+
+      if @thread
+        @thread.join
+        @thread = nil
+      end
     end
 
     def restart
       stop
       start
+    end
+
+    def flush
+      begin
+        @mutex.synchronize do
+          write
+        end
+      rescue Exception => ex
+        @on_error[ex] rescue nil
+      end
     end
 
     def submit
